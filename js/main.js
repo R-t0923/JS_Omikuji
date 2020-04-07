edfc2fd9ecf27098a10244ca8e533111f54400f8
R-t0923 <t.r712@gmail.com>
2020-04-07 14:07:05 +0900

    おみくじを表示してみよう

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@
   const bth = document.getElementById('bth');
 
   bth.addEventListener('click',()=>{
-    bth.textContent = 'hit!';
+    const n = Math.floor(Math.random() * 3);
+    // bth.textContent = n;
+
+
+    switch (n) {
+      case 0:
+        bth.textContent = "大吉";
+        break;
+      case 1:
+        bth.textContent = "中吉";
+        break;
+      case 2:
+        bth.textContent = "凶";
+        break;
+    }
   });
 }
